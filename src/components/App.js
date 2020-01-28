c4892649f58617e80f52a5775be5741d99d0320c
TravelFiend <mikegrace274@gmail.com>
2020-01-28 13:19:50 -0800

    header now componentized

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import styles from './App.css';
+import Header from './Header';
 
-const Header = () => (
-  <header className={styles.Header}>
-    <nav>
-      <ul>
-        <li><a className={styles.active} href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Documentation</a></li>
-      </ul>
-    </nav>
-    <section>
-      <h1>The Rick And Morty Api</h1>
-      <h2>Hey, did you ever want to hold a Terry fold?</h2>
-    </section>
-  </header>
-);
+// const Header = () => (
+//   <header className={styles.Header}>
+//     <nav>
+//       <ul>
+//         <li><a className={styles.active} href="#">Home</a></li>
+//         <li><a href="#">About</a></li>
+//         <li><a href="#">Documentation</a></li>
+//       </ul>
+//     </nav>
+//     <section>
+//       <h1>The Rick And Morty Api</h1>
+//       <h2>Hey, did you ever want to hold a Terry fold?</h2>
+//     </section>
+//   </header>
+// );
 
 const Footer = () => (
   <footer className={styles.Footer}>
