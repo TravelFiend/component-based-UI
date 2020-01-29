1db0b181f115a3dcc27a88aef81f38d0851c958b
TravelFiend <mikegrace274@gmail.com>
2020-01-28 22:39:10 -0800

    character cards written

---
+++ src/character.js
@@ -0,0 +1,90 @@
+import slotOne from '../assets/slotOne.jpeg';
+import slotTwo from '../assets/slotTwo.jpeg';
+import slotThree from '../assets/slotThree.jpeg';
+import slotFour from '../assets/slotFour.jpeg';
+import slotFive from '../assets/slotFive.jpeg';
+import slotSix from '../assets/slotSix.jpeg';
+import slotSeven from '../assets/slotSeven.jpeg';
+import slotEight from '../assets/slotEight.jpeg';
+
+const cardOne = {
+  image: { slotOne }, 
+  name: 'Abradolf Lincler',
+  status: 'unkown', 
+  species: 'Human, Genetic experiment',
+  gender: 'Male', 
+  origin: 'Earth (Replacement Dimension)',
+  lastLocation: 'Testicle Monster Dimension'
+};
+
+const cardTwo = {
+  image: { slotTwo },
+  name: 'Colossus',
+  status: 'Dead',
+  species: 'Human',
+  gender: 'Male',
+  origin: 'Post-Apocalyptic Earth',
+  lastLocation: 'Post-Apocalyptic Earth'
+};
+
+const cardThree = {
+  image: { slotThree },
+  name: 'Cornvelious Daniel',
+  status: 'Dead',
+  species: 'Alien, Gromflomite',
+  gender: 'Male',
+  origin: 'unknown',
+  lastLocation: 'Galactic Federation Prison'
+};
+
+const cardFour = {
+  image: { slotFour },
+  name: 'Mr. Marklovitz',
+  status: 'Alive',
+  species: 'Human',
+  gender: 'Male',
+  origin: 'Earth (C-137)',
+  lastLocation: 'Earth (Replacement Dimension)'
+};
+
+const cardFive = {
+  image: { slotFive },
+  name: 'Quantum Rick',
+  status: 'unknown',
+  species: 'Human',
+  gender: 'Male',
+  origin: 'unknown',
+  lastLocation: 'Citadel of Ricks'
+};
+
+const cardSix = {
+  image: { slotSix },
+  name: 'Samantha',
+  status: 'Alive',
+  species: 'Human',
+  gender: 'Female',
+  origin: 'Earth (C-137)',
+  lastLocation: 'Earth (C-137)'
+};
+
+const cardSeven = {
+  image: { slotSeven },
+  name: 'Zarbadar\'s Mytholog',
+  status: 'unknown',
+  species: 'Mytholog',
+  gender: 'Female',
+  origin: 'Nuptia 4',
+  lastLocation: 'Nuptia 4'
+};
+
+const cardEight = {
+  image: { slotEight },
+  name: 'Muscular Mannie',
+  status: 'Alive',
+  species: 'Human, Mannie',
+  gender: 'Male',
+  origin: 'Interdimensional Cable',
+  lastLocation: 'Interdimensional Cable'
+};
+
+export { cardOne, cardTwo, cardThree, cardFour, cardFive, cardSix, cardSeven, cardEight };
